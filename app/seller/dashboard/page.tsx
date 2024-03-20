8c061f3bcca789e457d00b18d05c006942720466
Dranee <adrianaviladiaz93@gmail.com>
2024-03-20 12:52:52 -0500

    update config

--- app/seller/dashboard/page.tsx
+++ app/seller/dashboard/page.tsx
@@ -1,23 +1,5 @@
-const DashboardPage = () => {
-  return (
-    <div className='w-full h-full flex items-center justify-center flex-col gap-6 animate'>
-      <img
-        className='max-h-[43%]'
-        src='https://res.cloudinary.com/dynscts1t/image/upload/v1710895154/g3cydxcfqkswfpgglqbp.svg'
-        alt='Sketch person practicating yoga'
-      />
-      <h1 className='max-w-lg text-2xl text-balance text-center font-medium '>
-        We are{' '}
-        <span className='before:block before:absolute before:-inset-1 before:-skew-y-1 before:bg-black relative inline-block'>
-          <span className='relative text-white'>working</span>
-        </span>{' '}
-        in this module; currently, the application are in{' '}
-        <span className='before:block before:absolute before:-inset-1 before:skew-y-1 before:bg-black relative inline-block'>
-          <span className='relative text-white'>demo version.</span>
-        </span>{' '}
-      </h1>
-    </div>
-  );
-};
+import WeAreWorking from '../we-are-working';
 
-export default DashboardPage;
+export default function DashboardPage() {
+  return <WeAreWorking />;
+}
---
+++ app/seller/we-are-working.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import React from 'react';
+
+const WeAreWorking = () => {
+  return (
+    <div className='w-full h-full flex items-center justify-center flex-col gap-6 animate relative'>
+      <Image
+        width={0}
+        height={0}
+        className='max-h-[43%] w-fit'
+        src='https://res.cloudinary.com/dynscts1t/image/upload/v1710895154/g3cydxcfqkswfpgglqbp.svg'
+        alt='Sketch person practicating yoga'
+      />
+      <h1 className='max-w-lg text-2xl text-balance text-center font-medium '>
+        We are{' '}
+        <span className='before:block before:absolute before:-inset-1 before:-skew-y-1 before:bg-black relative inline-block'>
+          <span className='relative text-white'>working</span>
+        </span>{' '}
+        in this module; currently, the application are in{' '}
+        <span className='before:block before:absolute before:-inset-1 before:skew-y-1 before:bg-black relative inline-block'>
+          <span className='relative text-white'>demo version.</span>
+        </span>{' '}
+      </h1>
+    </div>
+  );
+};
+
+export default WeAreWorking;
